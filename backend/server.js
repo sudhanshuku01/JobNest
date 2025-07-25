7b1f062737d0c1a40ba05629288cde09dd6cac13
Sudhanshu Kumar <sudhanshu620026@gmail.com>
2025-07-25 13:56:38 +0530

    url update3

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: ["https://jobsnest.netlify.app/"],
+    origin: ["https://jobsnest.netlify.app"],
     credentials: true,
   })
 );
